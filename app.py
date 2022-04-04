be0fd2834db4b5c74315e489538df5e78fd36535
MJonczyk <micjon1997@gmail.com>
2022-04-04 19:04:11 +0200

    registration and login endpoints

--- app.py
+++ app.py
@@ -1,28 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+cors = CORS(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), unique=False, nullable=False)
 
 
-
-@app.route('/create/<username>/<password>')
-def create(username, password):
-    user = User(username = username, password = password)
+@app.route('/register', methods=['POST'])
+def create():
+    data = request.get_json()
+    user = User(
+        username=data['username'],
+        email=data['email'],
+        password=data['password']
+    )
     db.session.add(user)
     db.session.commit()
-    return f"user <b>{username}</b> created successfully"
+    return jsonify(user)
 
 
-@app.route('/select/<username>')
-def select(username):
-    user = User.query.filter_by(username=username).first()
-    return f"The password is <b>{user.password}</b>"
+@app.route('/login', methods=['POST'])
+def select():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    return jsonify(username=username, password=password)
